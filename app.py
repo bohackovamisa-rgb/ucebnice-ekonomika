7996386d694ed475f67f1be68611a0b155039872
bohackovamisa-rgb <bohackova.misa@gmail.com>
2026-08-05 18:01:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -989,11 +989,12 @@ elif view == "Kapitola 1":
             elif sim_choice == "B) Udělám jednoduchý dotazník a jednoduchý web pro zájemce":
                 st.success("🎉 SKVĚLE! Získal jsi zdarma 500 zájemců a ověřil trh. Můžeš bezpečně stavět MVP!")
 
-           with st.expander("💡 Dvě otázky pro představivost"):
-                st.write("**Otázka 1: Proč většina startupů selže v prvním roce?**")
-                st.write("*Odpověď:* Vyrobí produkt, který ve skutečnosti nikdo nepotřebuje (neověří si poptávku).")
-                st.write("**Otázka 2: Co znamená zkratka MVP?**")
-                st.write("*Odpověď:* Minimum Viable Product – nejjednodušší verze produktu, která už funguje a dá se testovat na lidech.")
+          st.markdown("##### 💡 Dvě otázky pro představivost")
+            with st.expander("❓ Otázka 1: Proč většina startupů selže v prvním roce?"):
+                st.success("**Odpověď:** Vyrobí produkt, který ve skutečnosti nikdo nepotřebuje (neověří si poptávku).")
+            
+            with st.expander("❓ Otázka 2: Co znamená zkratka MVP?"):
+                st.success("**Odpověď:** Minimum Viable Product – nejjednodušší verze produktu, která už funguje a dá se testovat na lidech.")
 
             st.write("Startupová kultura je blízká dnešní generaci, protože spojuje technologie, sociální sítě, AI, komunitu, rychlé testování a možnost tvořit i s malým rozpočtem. Zároveň ale svádí k iluzi, že stačí dobrý nápad, virální video nebo hezká aplikace.")
             st.write("Ve skutečnosti startup stojí na ověřování:")
