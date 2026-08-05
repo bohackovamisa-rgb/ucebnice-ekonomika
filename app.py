b1b89d70aa669568a9034316650e9f2e6dae54a0
bohackovamisa-rgb <bohackova.misa@gmail.com>
2026-08-05 18:46:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="Učebnice Ekonomiky", layout="wide")
 
 st.title("Test aplikace")
-st.write("Pokud toto vidíte, Streamlit funguje správně!")
+st.write("Aplikace je živá!")
