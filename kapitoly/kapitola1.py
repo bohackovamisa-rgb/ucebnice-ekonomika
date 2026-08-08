f4acd45caeb1a3eb6b6f0179e705ddab3da51e00
bohackovamisa-rgb <bohackova.misa@gmail.com>
2026-08-08 18:35:37 +0200

    Update kapitola1.py

--- kapitoly/kapitola1.py
+++ kapitoly/kapitola1.py
@@ -24,8 +24,7 @@ def render():
         "15. Integrované opakování"
     ]
     st.markdown("<i class='fi fi-bs-bookmark'></i> <strong>Přechod na podkapitolu:</strong>", unsafe_allow_html=True)
-    selected_section = st.markdown("<i class='fi fi-bs-bookmark'></i> <strong>Přechod na podkapitolu:</strong>", unsafe_allow_html=True)
-    st.selectbox("Přechod na podkapitolu:", section_options, index=0, label_visibility="collapsed")
+    selected_section = st.selectbox("Přechod na podkapitolu:", section_options, index=0, label_visibility="collapsed")
     st.divider()
 
 # --- 1. Podnikatel a základní pojmy ---
