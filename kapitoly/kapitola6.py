9cbd6049356774de3b9543a30ab1e4e913bc22fd
bohackovamisa-rgb <bohackova.misa@gmail.com>
2026-08-08 20:00:05 +0200

    Update kapitola6.py

--- kapitoly/kapitola6.py
+++ kapitoly/kapitola6.py
@@ -125,14 +125,13 @@ def render():
         with tab_role1:
             st.markdown("##### Manažer")
             st.write("**Co dělá:** Řídí lidi, procesy nebo část organizace. Odpovídá za splnění cílů a efektivitu.")
-        st.markdown("📌 <strong>Přechod na podkapitolu:</strong>", unsafe_allow_html=True)
-        selected_section_6 = st.selectbox("Přechod na podkapitolu:", section_options_6, index=0, label_visibility="collapsed")
-        
+            st.info("📌 **Příklady:** Vedoucí týmu, ředitel školy, manažer prodejny.")
+
         with tab_role2:
             st.markdown("##### Podnikatel")
             st.write("**Co dělá:** Přichází s nápadem, vyhledává příležitosti na trhu, nese riziko a chce vytvořit novou hodnotu.")
             st.info("📌 **Příklady:** Zakladatel e-shopu, majitel kavárny, startupový founder.")
-
+       
         with tab_role3:
             st.markdown("##### Vlastník (Investor)")
             st.write("**Co dělá:** Vlastní firmu nebo její podíl (akcie). Dává do ní kapitál. Nemusí v ní ale vůbec pracovat ani ji denně řídit.")
