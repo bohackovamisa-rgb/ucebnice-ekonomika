fbc2fa6308d9a87d5360f9feb4a8d05dbce0e714
bohackovamisa-rgb <bohackova.misa@gmail.com>
2026-08-08 19:21:12 +0200

    Update kapitola5.py

--- kapitoly/kapitola5.py
+++ kapitoly/kapitola5.py
@@ -57,7 +57,7 @@ def render():
     ]
     
     st.markdown("📌 <strong>Přechod na podkapitolu:</strong>", unsafe_allow_html=True)
-    selected_section_5 = st.selectbox("Přechod na podkapitolu:", section_options_3, index=0, label_visibility="collapsed")
+    selected_section_5 = st.selectbox("Přechod na podkapitolu:", section_options_5, index=0, label_visibility="collapsed")
     st.divider()
 
     # =========================================================================
